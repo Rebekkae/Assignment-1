27ff25255bb3803cd6b75283148497ddf441e9c9
Rebekka Viktoria Mejlhede Eriksen <rebekkameriksen@gmail.com>
2022-09-11 17:06:00 +0200

    Implemented Flatten with test

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,5 +1,19 @@
 namespace Assignment1.Tests;
 
-public class IteratorsTests
-{
+public class IteratorsTests {
+
+    [Fact]
+
+    public void flatten_from_lists_1_2_and_3_4_return_list_of_1_2_3_4() {
+
+        // Arrange
+        IEnumerable<IEnumerable<int>> streamOfStream = new [] { new[] {1, 2}, new[] {3, 4} };
+
+        // Act
+        var result = Iterators.Flatten(streamOfStream);
+
+        // Assert
+        Assert.Equal(new[] {1, 2, 3, 4}, result);
+
+    }
 }
--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -1,8 +1,15 @@
 ﻿namespace Assignment1;
 
-public static class Iterators
-{
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+public static class Iterators {
+
+    public static void Main(string[] args) {}
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) {
+        foreach (IEnumerable<T> i in items) {
+             foreach (T j in i) {
+                yield return j; 
+             }
+        }
+    }
 
     public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
 }
