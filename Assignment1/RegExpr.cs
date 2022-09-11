fa7ac6b4968ecf9618fa228b104981a686ce1576
Rebekkae <76994939+Rebekkae@users.noreply.github.com>
2022-09-11 21:11:10 +0200

    Implemented SplitLine with test (#2)

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,8 +1,14 @@
 namespace Assignment1;
-
-public static class RegExpr
-{
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+using System.Text.RegularExpressions;
+public static class RegExpr {
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) {
+        string pattern = @"[a-zA-Z0-9]+";
+        foreach (string s in lines) {
+           foreach (Match match in Regex.Matches(s, pattern)) {
+                yield return match.ToString();
+            }
+        }
+    }
 
     public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
 
