d867b2c22cd1fae624ea4ff1e7b253894b76d26c
Rebekka Viktoria Mejlhede Eriksen <rebekkameriksen@gmail.com>
2022-09-12 17:30:28 +0200

    Added Resolution with test

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -14,4 +14,17 @@ public class RegExprTests {
         // Assert
         Assert.Equal(new[] {"this", "is", "a", "sentence", "This", "Is", "Also", "A", "Sentence", "100", "100", "gives", "200"}, result);
     }
+
+    [Fact]
+    public void resolution_when_given_4_resolutions_return_stream_of_4_tuples() {
+
+        // Arrange
+        string input = "1920x1080, 1024x768, 800x600, 640x480";
+
+        // Act
+        var result = RegExpr.Resolution(input);
+
+        // Assert
+        Assert.Equal(new[] { (1920, 1080), (1024, 768), (800, 600), (640, 480)}, result);
+    }
 }
