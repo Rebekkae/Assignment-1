98b22ee968a3de38f374a439e9922294573f9aaf
Rebekkae <76994939+Rebekkae@users.noreply.github.com>
2022-09-15 15:17:44 +0200

    Merging branch (#10)
    * -
    
    * -

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,4 +1,6 @@
 namespace Assignment1;
+
+using System.Collections;
 using System.Text.RegularExpressions;
 public static class RegExpr {
     public static IEnumerable<string> SplitLine(IEnumerable<string> lines) {
@@ -11,14 +13,59 @@ public static class RegExpr {
     }
 
     public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions) {
-        string pattern = @"( *(?<width>[0-9]*)[x ]*(?<height>[0-9]*) *)";
+        string pattern = @"(?<width>[0-9]*)x(?<height>[0-9]*)";
         foreach (string resolution in resolutions) {
             Match match = Regex.Match(resolution, pattern);
             if (match.Success) {
-                yield return (Int32.Parse(match.Groups["width"].Value), Int32.Parse(match.Groups["height"].Value));
+                yield return (int.Parse(match.Groups["width"].Value), int.Parse(match.Groups["height"].Value));
             }
         }
     }
 
-    public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+    public static IEnumerable<string> InnerText(string html, string tag){
+        //Console.WriteLine(html);
+        var pattern = $"<({tag})" + @"([^>]+)*>(.+?)<\/\1>";
+        var regex = new Regex(pattern);   
+        foreach (Match match in regex.Matches(html)){
+            yield return string.Join("", removeNestedTags(match.Value, tag)); 
+        } 
+
+    }
+
+    public static IEnumerable<string> removeNestedTags(string html, string tag){
+        var pattern = @"([\w ,()-.\/""]+(?![^<>]*>))";
+        var regex = new Regex(pattern);
+        foreach (Match match in regex.Matches(html)){
+            yield return match.Value; 
+        }
+    }
+
+    
+
+    public static IEnumerable<(Uri url, string title)> Urls(string html){
+        var masterPattern = @"(?<everything><(a)(?<attributes>[^>]+)*>(.+?)<\/a>)";
+        var pattern = @"([""'])(?<url>.*?)([""'])(\stitle=([""'])(?<title>.*?)([""']))?";
+        var regex = new Regex(masterPattern);
+        var url_regex = new Regex(pattern);
+        var matches = Regex.Match(html, masterPattern);
+        if (matches.Success){
+            foreach(Match match in regex.Matches(html)){
+                foreach (Match url in url_regex.Matches(match.Groups["attributes"].Value)){
+                    if (url.Groups["url"].Success){
+                        if (url.Groups["title"].Success){
+                            yield return (new Uri(url.Groups["url"].Value), url.Groups["title"].Value);
+                        }
+                        else{
+                            string inner = InnerText(match.Groups["everything"].Value, "a").First<string>();
+                            yield return (new Uri(url.Groups["url"].Value), inner);
+                        }
+                    }
+                }
+                
+            }
+        }
+        else{
+            throw new ArgumentException("URL not found");
+        }  
+    }
 }
