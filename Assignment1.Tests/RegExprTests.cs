e568a70658360a596c050ab2527794665b2d3f7f
lukas175 <lukasersej175@gmail.com>
2022-09-15 13:20:58 +0200

    innertext and url implementation + test added

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -16,6 +16,20 @@ public class RegExprTests {
     }
 
     [Fact]
+    public void splitline_given_special_characters_return_empty()
+    {
+
+        // Arrange
+        IEnumerable<string> stream = new[] { " , ", " ? ", " ! ", " . "};
+
+        // Act
+        var result = RegExpr.SplitLine(stream);
+
+        // Assert
+        Assert.Equal(Array.Empty<string>(), result);
+    }
+
+    [Fact]
     public void resolution_when_given_4_resolutions_return_stream_of_4_tuples() {
 
         // Arrange
@@ -26,5 +40,44 @@ public class RegExprTests {
 
         // Assert
         Assert.Equal(new[] { (1920, 1080), (1024, 768), (800, 600), (640, 480)}, result);
+    }
+
+    [Fact]
+    public void innerText_given_tag_p_should_return(){
+        //Arrange
+        string lines = "<div>\n<p>The phrase <i>regular expressions</i> (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing <u>patterns</u> that matching <em>text</em> need to conform to.</p>\n</div>";
+
+        //Act
+        var expected = new List<string>(){"The phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to."};
+        var result = RegExpr.InnerText(lines, "p");
+        //Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void innerText_given_tag_a_should_return(){
+        //Arrange
+        string lines = @"<div>\n<p>A <b>regular expression</b>, 
+        <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) 
+        is, in <a href=""https://en.wikipedia.org/wiki/Theoretical_computer_science"" 
+        title=""Theoretical computer science"">theoretical computer science</a> and 
+        <a href=""https://en.wikipedia.org/wiki/Formal_language"" 
+        title=""Formal language"">formal language</a> theory, a sequence of 
+        <a href=""https://en.wikipedia.org/wiki/Character_(computing)"" 
+        title=""Character (computing)"">characters</a> that define a <i>search 
+        <a href=""https://en.wikipedia.org/wiki/Pattern_matching"" 
+        title=""Pattern matching"">pattern</a></i>. Usually this pattern is then 
+        used by <a href=""https://en.wikipedia.org/wiki/String_searching_algorithm"" 
+        title=""String searching algorithm"">string searching algorithms</a> for 
+        ""find"" or ""find and replace"" operations on 
+        <a href=""https://en.wikipedia.org/wiki/String_(computer_science)"" 
+        title=""String (computer science)"">strings</a>.</p>\n</div>";
+
+        //Act
+        var expected = new List<string>(){"theoretical computer science", 
+        "formal language", "characters", "pattern", "string searching algorithms", "strings"};
+        var result = RegExpr.InnerText(lines, "a");
+        //Assert
+        Assert.Equal(expected, result);
     }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -15,10 +15,55 @@ public static class RegExpr {
         foreach (string resolution in resolutions) {
             Match match = Regex.Match(resolution, pattern);
             if (match.Success) {
-                yield return (Int32.Parse(match.Groups["width"].Value), Int32.Parse(match.Groups["height"].Value));
+                yield return (int.Parse(match.Groups["width"].Value), int.Parse(match.Groups["height"].Value));
             }
         }
     }
 
-    public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+    public static IEnumerable<string> InnerText(string html, string tag){
+        Console.WriteLine(html);
+        var pattern = $"<({tag})" + @"([^>]+)*>(.+?)<\/\1>";
+        var regex = new Regex(pattern);   
+        foreach (Match match in regex.Matches(html)){
+            yield return string.Join("", removeNestedTags(match.Value, tag)); 
+        }
+        
+    }
+
+    public static IEnumerable<string> removeNestedTags(string html, string tag){
+        var pattern = @"([\w ,()-.\/""]+(?![^<>]*>))";
+        var regex = new Regex(pattern);
+        foreach (Match match in regex.Matches(html)){
+            yield return match.Value; 
+        }
+    }
+
+    public static IEnumerable<(Uri url, string title)> Urls(string html){
+        var masterPattern = @"(?<everything><(a)(?<attributes>[^>]+)*>(.+?)<\/\2>)";
+        var pattern = @"([""'])(?<url>.*?)\1(\stitle=\1(?<title>.*?)\1)?";
+        var regex = new Regex(masterPattern);
+        var url_regex = new Regex(pattern);
+        var matches = Regex.Match(html, masterPattern);
+        if (matches.Success){
+            foreach(Match match in regex.Matches(html)){
+                foreach (Match url in url_regex.Matches(match.Groups["attributes"].Value)){
+                    if (url.Groups["url"].Success){
+                        Console.WriteLine("url match");
+                        if (url.Groups["title"].Success){
+                            yield return (new Uri(url.Groups["url"].Value), url.Groups["title"].Value);
+                        }
+                        else{
+                            Console.WriteLine("no title match");
+                            string inner = InnerText(match.Groups["everything"].Value, "a").First<string>();
+                            yield return (new Uri(url.Groups["url"].Value), inner);
+                        }
+                    }
+                }
+                
+            }
+        }
+        else{
+            throw new ArgumentException("URL not found");
+        }  
+    }
 }
